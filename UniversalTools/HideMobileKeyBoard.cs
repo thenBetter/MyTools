ad8360a0aa87da7f78ca25111a0c04911d3357bd
zhangfeng <1535477644@qq.com>
2019-06-27 12:56:08 +0800

    ""

---
+++ UniversalTools/HideMobileKeyBoard.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+/// <summary>
+/// 屏蔽移动端弹出系统键盘
+/// </summary>
+public class HideMobileKeyBoard : InputField
+{
+    protected override void Start()
+    {
+        keyboardType = (TouchScreenKeyboardType)(-1);
+        base.Start();
+    }
+
+    protected override void LateUpdate()
+    {
+        try
+        {
+            base.LateUpdate();
+        }
+        catch (System.Exception e)
+        {
+#if UNITY_EDITOR
+          Debug.Log(e.Message);
+#endif
+        }
+
+    }
+}
