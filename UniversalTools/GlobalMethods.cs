8d876c9cb22e1868135166f41e1294386bfde1a8
ThenBetter <1535477644@qq.com>
2020-08-17 22:53:00 +0800

    get raycasthit

--- UniversalTools/GlobalMethods.cs
+++ UniversalTools/GlobalMethods.cs
@@ -819,9 +819,20 @@ namespace UniversalTools
             return point;
         }
 
+        public static bool GetCurrentCollisionAtMousePosition(Camera camera, out RaycastHit hit, float distance = -1)
+        {
+            if (camera != null)
+            {
+                Ray ray = camera.ScreenPointToRay(Input.mousePosition);
+                float d = distance > 0f ? distance : camera.farClipPlane;
+                if (Physics.Raycast(ray,  out hit, d, camera.cullingMask))
+                {
+                    return true;
+                }   
+            }
+            hit = new RaycastHit();
+            return false;
+        }
+
     }
-
-
-
-
 }
